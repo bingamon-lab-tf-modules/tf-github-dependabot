d0983070d8c0b08c96ad3a8966e50d7430ec1db4
MAHDTech <MAHDTech@saltlabs.tech>
2025-06-26 15:06:51 +1000

    chore: add tests (#3)

--- tests/main.tf
+++ tests/main.tf
@@ -1,6 +1,34 @@
 module "test" {
   source = "../module"
 
-  # TF Variables here...
+  providers = {
+    github = github.organization
+  }
+
+  github_enterprise_slug   = "acme-corp"
+  github_organization_name = "acme-engineering"
+
+  github_repository_data = {
+    "acme-engineering/acme-service" = {
+      name                 = "acme-service"
+      vulnerability_alerts = true
+    }
+  }
+
+  github_dependabot_enabled_repositories = [
+    {
+      name    = "acme-engineering/acme-service"
+      enabled = true
+    }
+  ]
+
+  github_dependabot_secrets = [
+    {
+      name            = "acme-engineering/acme-service"
+      type            = "organization"
+      encrypted_value = "c2VjcmV0X3ZhbHVl"
+      visibility      = "all"
+    }
+  ]
 
 }
