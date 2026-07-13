76c8bc30f22ff3b35cceaed56a63e8d0b904a8ef
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-13 22:30:59 +1000

    chore: migrate spell checker to cspell

--- devenv.nix
+++ devenv.nix
@@ -142,6 +142,13 @@ in
       check-yaml.enable = true;
       commitizen.enable = true;
       convco.enable = true;
+      cspell = {
+        enable = true;
+        args = [
+          "lint"
+          "--no-must-find-files"
+        ];
+      };
       deadnix.enable = true;
       dialyzer.enable = true;
       editorconfig-checker.enable = true;
@@ -206,7 +213,6 @@ in
       tflint.enable = true;
       trim-trailing-whitespace.enable = true;
       trufflehog.enable = false;
-      typos.enable = true;
       yamllint = {
         enable = true;
         settings = {
@@ -254,7 +260,6 @@ in
             "skellock.just"
             "streetsidesoftware.code-spell-checker"
             "tamasfe.even-better-toml"
-            "tekumura.typos-vscode"
             "timonwong.shellcheck"
             "tuxtina.json2yaml"
             "vscodevim.vim"
